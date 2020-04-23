a30278b4ca577302c9f0eb391073afe3276da291
SailentzZz <mishatravin99@gmail.com>
2020-04-24 01:35:06 +0300

    first commit

---
+++ Assets/ChangeScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel("Prax_10_1");
+        }
+           if(Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel("Prax_10_2");
+        }
+    }
+}
